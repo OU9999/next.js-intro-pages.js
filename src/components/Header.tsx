920d7f490cab412cc5fdd0e3303ed56183798ec5
오유진 <omh232323@gmail.com>
2023-03-29 13:43:24 +0900

    settings

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,26 @@
-import { Box, Button, HStack } from "@chakra-ui/react";
+import { Button, HStack } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <HStack
-      px="10"
-      py="3"
-      gap={3}
-      w="full"
-      position="fixed"
-      zIndex={99}
-      bgColor="#fff"
-      boxShadow={"dark-lg"}
-    >
-      <Link href="/">
-        <Button>Home</Button>
-      </Link>
-      <Link href="/about">
-        <Button>about</Button>
-      </Link>
-    </HStack>
+    <>
+      <HStack
+        px="10"
+        py="3"
+        gap={3}
+        w="full"
+        position="fixed"
+        zIndex={99}
+        bgColor="#fff"
+        boxShadow={"dark-lg"}
+      >
+        <Link href="/">
+          <Button>Home</Button>
+        </Link>
+        <Link href="/about">
+          <Button>about</Button>
+        </Link>
+      </HStack>
+    </>
   );
 }
